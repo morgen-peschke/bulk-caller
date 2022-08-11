c130c5e039afa11b97837d2f34f7ea9129b58c18
Morgen Peschke <morgen.peschke@gmail.com>
2022-08-11 16:18:05 -0700

    Add documentation and tests to JsonPath

--- src/main/scala/peschke/bulk_calls/models/JsonPath.scala
+++ src/main/scala/peschke/bulk_calls/models/JsonPath.scala
@@ -2,6 +2,7 @@ package peschke.bulk_calls
 package models
 
 import cats.Show
+import cats.kernel.Semigroup
 import cats.parse.{Numbers, Parser}
 import cats.syntax.either._
 import cats.syntax.eq._
@@ -9,8 +10,18 @@ import cats.syntax.show._
 import io.circe.Decoder.Result
 import io.circe.{CursorOp, Decoder, Encoder, Json}
 
+/**
+  * Represents a path into a JSON graph
+  *
+  * @param segments each segment is a a movement one step deeper into the JSON graph
+  *                 These may be:
+  *                   - an identifier-style field, like `.foo`
+  *                   - an index-style field, like `["foo"]`
+  *                   - an array index, like `[1]`
+  * @param raw the raw versions, saved to make it easy to make serialization stable.
+  */
 final case class JsonPath(segments: List[CursorOp], raw: String) {
-  def extract(json: Json): Result[Option[Json]] =
+  def extract(json: Json): Result[Option[Json]]g =
     json.hcursor.replay(segments).as[Option[Json]]
 }
 object JsonPath {
@@ -21,16 +32,16 @@ object JsonPath {
     val identifier: Parser[String] = {
       val initial = Parser.charWhere(_.isLetter).orElse(Parser.char('_'))
       val rest = initial.orElse(Numbers.digit)
-      Parser.char('.') *> (initial *> rest.rep0).string
+      Parser.char('.') *> (initial *> rest.rep0).string.withContext("method-style identifier")
     }
 
     val explicitField: Parser[String] =
-      Parser.until(quote).surroundedBy(quote)
+      Parser.until(quote).surroundedBy(quote).withContext("index-style field")
 
-    val arrayIndex: Parser[String] = Numbers.nonNegativeIntString
+    val arrayIndex: Parser[String] = Numbers.nonNegativeIntString.withContext("array index")
 
     val cursorOp: Parser[CursorOp] = {
-      val downFieldSimple: Parser[CursorOp] = identifier.map(CursorOp.DownField)
+      val downFieldSimple: Parser[CursorOp] = identifier.map(CursorOp.DownField).withContext("field")
       val downFieldOrArrayComplex: Parser[CursorOp] =
         explicitField.map(CursorOp.DownField)
           .orElse(arrayIndex.mapFilter(_.toIntOption).map(CursorOp.DownN))
@@ -47,6 +58,17 @@ object JsonPath {
     else parsers.path.parseAll(raw).map(JsonPath(_, raw))
 
   implicit final val show: Show[JsonPath] = Show.show(_.raw)
+
+  implicit final val semigroup: Semigroup[JsonPath] = Semigroup.instance { (lhs, rhs) =>
+    JsonPath(
+      lhs.segments ::: rhs.segments,
+      (lhs.raw, rhs.raw) match {
+        case (".", r) => r
+        case (l, ".") => l
+        case (l, r) => s"$l$r"
+      }
+    )
+  }
 
   implicit final val decoder: Decoder[JsonPath] = Decoder[String].emap {
     from(_).leftMap(_.show)
---
+++ src/test/scala/peschke/bulk_calls/models/JsonPathTest.scala
@@ -0,0 +1,126 @@
+package peschke.bulk_calls.models
+
+import cats.syntax.either._
+import io.circe.CursorOp.{DownField, DownN}
+import org.scalacheck.{Arbitrary, Gen}
+import org.scalatest.matchers.{MatchResult, Matcher}
+import peschke.bulk_calls.PropertyTest.syntax._
+import peschke.bulk_calls.models.JsonPathTest.{arrayIndexes, complexPaths, indexFieldsWithUnescapedQuote, indexFieldsWithUnmatchedBraces, indexFieldsWithUnmatchedQuote, indexStyleFields, invalidMethodFields, methodStyleFields, negativeArrayIndexes}
+import peschke.bulk_calls.{DefaultTestInstances, WordTest}
+
+import scala.util.matching.Regex
+
+class JsonPathTest extends WordTest {
+  val parseSuccessfully: Matcher[JsonPath] = Matcher { path =>
+    be(path.asRight).apply(JsonPath.from(path.raw))
+  }
+
+  val failToParse: Matcher[String] = Matcher { input =>
+    val result = JsonPath.from(input)
+    MatchResult(
+      result.isLeft,
+      "JsonPath.from({0}) did not fail, but returned {1}",
+      "JsonPath.from({0}) failed with error {1}",
+      Vector(input, result),
+      Vector(input, result)
+    )
+  }
+
+  "JsonPath.from" should {
+    "parse method-style fields" in forAll(methodStyleFields)(_ must parseSuccessfully)
+
+    "parse index-style fields" in forAll(indexStyleFields)(_ must parseSuccessfully)
+
+    "parse array indexes" in forAll(arrayIndexes)(_ must parseSuccessfully)
+
+    "parse a single '.'" in {
+      JsonPath.from(".").value mustBe JsonPath(Nil, ".")
+    }
+
+    "parse complex paths" in forAll(complexPaths)(_ must parseSuccessfully)
+
+    "reject method-style fields with invalid characters" in forAll(invalidMethodFields)(_ must failToParse)
+
+    "reject index-style fields with unescaped quotes" in forAll(indexFieldsWithUnescapedQuote)(_ must failToParse)
+
+    "reject negative array indexes" in forAll(negativeArrayIndexes)(_ must failToParse)
+
+    "reject unmatched quotes" in forAll(indexFieldsWithUnmatchedQuote)(_ must failToParse)
+
+    "reject unmatched braces" in forAll(indexFieldsWithUnmatchedBraces)(_ must gfailToParse)
+  }
+}
+object JsonPathTest extends DefaultTestInstances {
+  private val validLeadingCharForMethodStyleFields = Gen.oneOf(Gen.alphaChar, Gen.const('_'))
+  private val validTrailingCharForMethodStyleFields = Gen.oneOf(Gen.alphaNumChar, Gen.const('_'))
+
+  private val unescapedQuoteRegex = raw"""(\\*)"""".r
+  private def escapedIndexStyleFieldName(length: Range): Gen[String] =
+    Arbitrary.arbitrary[Char].string(length)
+      .map(unescapedQuoteRegex.replaceAllIn(_, _ match {
+        case Regex.Groups(escapes) =>
+          if (escapes.length % 2 == 0) raw"""$$1\"""" else raw"""$$1""""
+      }))
+
+  val methodStyleFields: Gen[JsonPath] =
+    for {
+      c0 <- validLeadingCharForMethodStyleFields
+      cN <- validTrailingCharForMethodStyleFields.string(0 to 20)
+    } yield {
+      val field = cN.prepended(c0)
+      JsonPath(DownField(field) :: Nil, field.prepended('.'))
+    }
+
+  val indexStyleFields: Gen[JsonPath] =
+    escapedIndexStyleFieldName(1 to 20).map { fieldName =>
+      JsonPath(DownField(fieldName) :: Nil, s"""["$fieldName"]""")
+    }
+
+  val arrayIndexes: Gen[JsonPath] =
+    (0 to Int.MaxValue).gen.map { index =>
+      JsonPath(DownN(index) :: Nil, s"[$index]")
+    }
+
+  val complexPaths: Gen[JsonPath] = {
+    val subPaths = Gen.oneOf(methodStyleFields, indexStyleFields, arrayIndexes)
+    subPaths.nel(1 to 10).map(_.reduce)
+  }
+
+  val invalidMethodFields: Gen[String] = {
+    val badChars = Gen.oneOf("$&[{}(=*)+]!")
+    val invalidLeadingChar =
+      for {
+        first <- Gen.oneOf(Gen.numChar, badChars)
+        rest <- validTrailingCharForMethodStyleFields.string(0 to 10)
+      } yield s".$first$rest"
+
+    val invalidTrailingChar =
+      for {
+        first <- validLeadingCharForMethodStyleFields
+        prefix <- validTrailingCharForMethodStyleFields.string(0 to 5)
+        badChar <- badChars
+        suffix <- validTrailingCharForMethodStyleFields.string(0 to 5)
+      } yield s".$first$prefix$badChar$suffix"
+
+    val missingPeriod = methodStyleFields.map(_.raw.drop(1))
+
+    Gen.oneOf(missingPeriod, invalidLeadingChar, invalidTrailingChar)
+  }
+
+  val indexFieldsWithUnescapedQuote: Gen[String] =
+    for {
+      prefix <- escapedIndexStyleFieldName(0 to 5)
+      suffix <- escapedIndexStyleFieldName(0 to 5)
+    } yield s"""$prefix"$suffix"""
+
+  val negativeArrayIndexes: Gen[String] = (Int.MinValue to -1).gen.map(index => s"[$index]")
+
+  val indexFieldsWithUnmatchedQuote: Gen[String] =
+    escapedIndexStyleFieldName(1 to 20).map(name => s"""["$name]""")
+
+  val indexFieldsWithUnmatchedBraces: Gen[String] =
+    Gen.oneOf(
+      escapedIndexStyleFieldName(1 to 20).map(name => s"""["$name""""),
+      (0 to Int.MaxValue).gen.map(index => s"[$index")
+    )
+}
